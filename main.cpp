31b970d759548f55a7fddc93f4f838d5719cd90d
zhangkang <zhang15188365429@outlook.com>
2020-03-10 22:03:12 +0800

    modified

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include<string>
 using namespace std;
 void painter(const string &a){
@@ -15,10 +16,32 @@ void printRect(int width, int height){
         cout<<endl;
         
     }
+}
+void printcircle(int r){
+    int pointx=r;
+    int pointy=r;
+    for (int i = 0; i < 2*r; i++)
+    {
+        for (int j = 0; j < 2*r; j++)
+        {
+            int tempa=pow(i-pointx,2);
+            int tempb=pow(j-pointy,2);
+            float distance=sqrt(tempa+tempb);
+            if (distance<=r)
+            {
+                cout<<"&";
+            }else 
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
     
 }
 int main(){
     painter("hello");
     printRect(20,15);
+    printcircle(20);
     return 0;
 }
